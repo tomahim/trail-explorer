c83497b77713b385d5fa9038434388226240f738
Thomas Himblot <thimblot@gmail.com>
2025-03-04 22:30:46 +0100

    Add basic App.js component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Trail Explorer</h1>
+        <p>Discover and explore trail running routes</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
